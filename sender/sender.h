03c024cde1eed7e01d0875a0317b62869c80519c
kelolemon <cutesinapis@gmail.com>
2020-10-26 19:38:40 +0800

    usual update

--- sender/sender.h
+++ sender/sender.h
@@ -10,5 +10,27 @@
 # include <string>
 # include <cstring>
 # include <filesystem>
+
+# include <sys/socket.h>
+# include <cstdio>
+# include <cstdlib>
+# include <cstring>
+# include <unistd.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+# include <csignal>
+# include <cerrno>
+# include <pthread.h>
 extern char filename[255];
+
+typedef struct
+{
+    struct sockaddr_in rev_address;
+    struct sockaddr_in send_address;
+    int total_bytes;
+    int bag_number;
+    int checksum;
+    int TTL;
+} Header;
 #endif //MULTI_LINUX_DATA_TRANSMISSION_SYSTEM_SENDER_H
--- sender/sender_init.h
+++ sender/sender_init.h
@@ -16,13 +16,4 @@
 # include <cerrno>
 # include <pthread.h>
 extern void sender(int sender_socket);
-typedef struct
-{
-    struct sockaddr_in rev_address;
-    struct sockaddr_in send_address;
-    int total_bytes;
-    int bag_number;
-    int checksum;
-    int TTL;
-} Header;
 #endif //MULTI_LINUX_DATA_TRANSMISSION_SYSTEM_SENDER_INIT_H
